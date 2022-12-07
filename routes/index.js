ad70a3099fd65c2497e3478b7362bc6bfd4d0397
David Zonana <davidzn2@gmail.com>
2022-12-06 19:23:49 -0600

    Hello world cambiado

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.send({
-    'it is alive': true
+    'hello world': 'Hola mundo'
   });
 });
 
